ee732bf0318d2740ff166e5167ba41601ec69be0
zhangqiang <44993003+JACK-ZHANG-coming@users.noreply.github.com>
2022-12-08 23:17:32 +0800

    init;

---
+++ src/Home/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './index.less';
+
+const Home = ()=>{
+
+  return (
+    <div className="home-page">
+      <div className="content-box">
+        <div className="content-box-water">111</div>
+      </div>
+      <div className="input-box">
+        222
+      </div>
+    </div>
+  )
+};
+
+export default Home
